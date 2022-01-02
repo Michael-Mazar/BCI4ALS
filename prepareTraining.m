a96df8c5b1f076d67bd85a3c55a8299eab6fafac
Nadav Am-Shalom <nadav.am-shalom@mail.huji.ac.il>
2022-01-02 13:46:50 +0200

    added function that finds repeted trails in training vec

--- prepareTraining.m
+++ prepareTraining.m
@@ -4,4 +4,28 @@ trainingVec = (1:numConditions);
 trainingVec = repmat(trainingVec,1,numTrials);
 trainingVec = trainingVec(randperm(length(trainingVec)));
 
+%% check for unwanted patterns
+MAX_TRY = 10;% try to fix maximum of 10 times
+thresh = 3;
+for i = 1:1:MAX_TRY 
+    changed = 0;
+    repet = 1;
+    cur_repet = trainingVec(1); % the current class
+    for j = 2:1:length(trainingVec)
+        if (trainingVec(j) == cur_repet) %% see if same as last
+            repet = repet + 1; %% adds one to repet if 
+            if (repet == thresh)
+                changed = 1;
+                break;
+            end 
+        else
+            cur_repet = trainingVec(j);
+            repet = 1;
+        end
+    end
+    if (changed == 0) %% nothing changed
+        break;
+    else %% try new permutation
+        trainingVec = trainingVec(randperm(length(trainingVec)));
+    end
 end
