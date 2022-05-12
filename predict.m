a12080288c1483eea106d792a3a841f5d52e57d3
Osher Maayan <34490668+oshermaayan@users.noreply.github.com>
2022-05-12 12:07:52 +0300

    Add prediction
    1. Add Python code (classifier, README, requirements file, saved model, etc.)
    2. Integrate Python code (classifier) in Main Online

---
+++ predict.m
@@ -0,0 +1,9 @@
+function [prediction] = predict(X)
+% X is a features matrix (or vector)
+
+prediction = pyrunfile(".\pythonTest\Classifier.py", ...
+                        "prediction", action="predict", datapoints=X);
+prediction = uint8(double(prediction));
+
+end
+
