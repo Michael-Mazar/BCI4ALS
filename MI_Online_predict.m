b15464de9b91c6df21088836627c2dbf8ba3318a
raz-perry <perryraz38@gmail.com>
2022-05-30 16:37:36 +0300

    implement predict gui on real EEG data
    Restore original saved model path

--- MI_Online_predict.m
+++ MI_Online_predict.m
@@ -2,21 +2,18 @@
 %% Refresh
 clc; clear; close all;
 %% Parameters
-
-% TODO: remove the manual recordingFolder
-recordingFolder = '/Users/osherm/Desktop/HUJI/BCI/BCI4ALS/recordings';
+recordingFolder = 'C:\Users\Raz\BCI4ALS\Recordings\ONLINE_TEST';
 config_param 
-dataFolder = '/Users/osherm/Desktop/HUJI/BCI/BCI4ALS/data';
-% addpath(string(lslPath));     % lab streaming layer library
-% addpath(string(lslPath) + '\bin'); % lab streaming layer bin
-
-% addpath(string(eeglabPath)); 
-% eeglab;
+dataFolder = 'C:\Users\Raz\GitRepos\BCI4ALS\data\combined';
+addpath(string(lslPath));     % lab streaming layer library
+addpath(string(lslPath) + '\bin'); % lab streaming layer bin
+addpath(string(eeglabPath)); 
+eeglab nogui;
 
 % Setup Python interperter
 % TODO: consider adding Python interperter path to config_params
 try
-    pyenv('Version','/Users/osherm/Desktop/HUJI/BCI/BCI4ALS/pythonTest/venv/bin/python');
+    pyenv('Version','C:\Users\Raz\anaconda3\envs\BCI\python.exe');
 
 catch e
     disp(e);
@@ -40,22 +37,25 @@ decCount = 0;
 successCount = 0;
 onlineTrainingVec = prepareTraining(online_trails,MI1params.numClasses);    % vector with the conditions for each trial
 numChans = size(EEG_chans,1);
+MI2params.offline = 0;
+MI2params.plot = 0;
+MI4params.offline = 0;
 
 %% Start LSL
 %% open stream
-% disp('Loading the Lab Streaming Layer library...');
-% lib = lsl_loadlib();                    % load the LSL library
-% disp('Opening Marker Stream...');
-% info = lsl_streaminfo(lib,'MarkerStream','Markers',1,0,'cf_string','myuniquesourceid23443');
-% outletStream = lsl_outlet(info);        % create an outlet stream using the parameters above
-% disp('Resolving an EEG Stream...');
-% result = {};
-% lslTimer = tic;
-% while isempty(result)
-%     result = lsl_resolve_byprop(lib,'type','EEG'); 
-% end
-% disp('Success resolving!');
-% EEG_Inlet = lsl_inlet(result{1});
+disp('Loading the Lab Streaming Layer library...');
+lib = lsl_loadlib();                    % load the LSL library
+disp('Opening Marker Stream...');
+info = lsl_streaminfo(lib,'MarkerStream','Markers',1,0,'cf_string','myuniquesourceid23443');
+outletStream = lsl_outlet(info);        % create an outlet stream using the parameters above
+disp('Resolving an EEG Stream...');
+result = {};
+lslTimer = tic;
+while isempty(result)
+    result = lsl_resolve_byprop(lib,'type','EEG'); 
+end
+disp('Success resolving!');
+EEG_Inlet = lsl_inlet(result{1});
 
 %% Start TCP connection
 HOST="localhost";
@@ -73,43 +73,40 @@ RUN_SERVER = true;
 
 while RUN_SERVER
     request = readline(server);
-    
     if request=="START"
-           
-%         trialStart = tic;
-%         buffer = [];
-%         while size(buffer,2) <= trialLength*fs
-%             cur_data = EEG_Inlet.pull_chunk();
-%             pause(0.1);
-%             if ~isempty(cur_data)
-%                 buffer = [buffer, cur_data];
-%             else
-%                 disp(strcat('no data to pull in trial', num2str(trial), 'after', num2str(toc(start)), 'seconds'));
-%             end
-%         end
+        cur_data = EEG_Inlet.pull_chunk();
+        pause(0.1);
+        trialStart = tic;
+        buffer = [];
+        while size(buffer,2) <= trialLength*fs
+            cur_data = EEG_Inlet.pull_chunk();
+            pause(0.1);
+            if ~isempty(cur_data)
+                buffer = [buffer, cur_data];
+            else
+                disp(strcat('no data to pull after ', num2str(toc(trialStart)), 'seconds'));
+            end
+        end
 % 
-%         EEG = pop_importdata('dataformat', 'matlab', 'nbchan', numChans,...
-%                         'data', buffer, 'srate', fs, 'pnts', 0, 'xmin', 0);
-% 
-%         %outletStream.push_sample(endTrial);         % needed???
-%         decCount = decCount + 1;
-%         MI2params.offline = 0;
-%         [EEG_arr] = preprocess(EEG, recordingFolder, eeglabPath, unused_channels, MI2params);
-%         EEG_data = EEG_arr(6).data;
-%         MIData = [];
-%         for channel=1:numChans
-%             MIData(1, channel, :) = EEG_data(channel, :);
-%         end
-%         [MIFeatures] = feature_engineering(recordingFolder, MIData, bands, times, W, MI4params, feature_setting);
+        EEG = pop_importdata('dataformat', 'matlab', 'nbchan', numChans,...
+                        'data', buffer, 'srate', fs, 'pnts', 0, 'xmin', 0);
+
+        decCount = decCount + 1;
+        [EEG_arr] = preprocess(EEG, recordingFolder, eeglabPath, unused_channels, MI2params);
+        EEG_data = EEG_arr(end).data;
+        MIData = [];
+        for channel=1:numChans
+            MIData(1, channel, :) = EEG_data(channel, :);
+        end
+        [MIFeatures] = feature_engineering(recordingFolder, MIData, bands, times, W, MI4params, feature_setting);
 %     
 %         % TODO: remove the following line
 %         SelectedIdx = 1:10;
-%         FeaturesSelected = MIFeatures(:, SelectedIdx);
+        FeaturesSelected = MIFeatures(:, SelectedIdx);
         %%% If Matlab doing predict:
-        %myPrediction(decCount) = trainedModel.predictFcn(FeaturesSelected); % TODO: load trained model!!
         
-        features = randi(5,1,10);
-        prediction = predict(features);
+%         features = randi(5,1,10);
+        prediction = predict(FeaturesSelected);
     
         write(server, int2str(prediction), "string");     
     
