30f15257421f3c0327b761688df7254b801c9ea5
Osher Maayan <34490668+oshermaayan@users.noreply.github.com>
2022-06-01 18:23:38 +0300

    Display images for predictions
    Remove dummy prediction printing, show images instead

--- gui/main.py
+++ gui/main.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+from typing import Dict
 os.path.dirname(sys.executable)
 
 # from tkinter import Label
@@ -15,12 +16,13 @@ from kivymd.uix.screen import MDScreen
 from kivymd_extensions.akivymd.uix.progresswidget import AKCircularProgress
 from kivymd.uix.behaviors.toggle_behavior import MDToggleButton
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.uix.image import Image
+from kivy.uix.floatlayout import FloatLayout
 # Import game;
 from game import *
 
 # Client-related imports
 from client import Client
-from threading import Thread
 import time
 
 class MyToggleButton(MDFillRoundFlatIconButton, MDToggleButton):
@@ -34,35 +36,50 @@ class ApplicationScreen(Screen):
     pass
 
 
-#TODO: remove PredictionScreen later
+
 class PredictionScreen(Screen):
-    def __init__(self, **kwargs):
+    def __init__(self, class_to_results: Dict[int, str], images_base_path: str, **kwargs):
         super(PredictionScreen,self).__init__(**kwargs)
 
-        self.print_thread = None
-        self.is_printing = False
         self.client = Client()
-
-    def printer(self):
-        while self.is_printing:
-            prediction = self.client.get_prediction_data()
-            self.ids["debugarea"].text += f'Prediction: {prediction}' + '\n'
-            time.sleep(1)
-
-    def do_print(self):
-        self.client.connect()
-
-        if not self.is_printing:
-            self.is_printing = True
-            self.print_thread = Thread(target=self.printer)
-            self.print_thread.start()
-        else:
-            self.is_printing = False
-            self.print_thread.join()
-            self.print_thread = None
+        
+        self.images_paths = self._create_images_paths(images_base_path)
+        self.ids["prediction_image"].source = self.images_paths["start"]
+        
     
-    def close_connection(self):
+    def _create_images_paths(self, images_base_path) -> Dict[str, str]:
+        """
+        Returns a mapping from the a result name (e.g., "yes") to its respective image path
+        """
+        # "wait" is currently not used
+        results = ["start", "wait", "yes", "no"]
+
+        images_paths = {}
+        for res in results:
+            images_paths[res] = "{}/{}.jpeg".format(images_base_path, res)
+
+        return images_paths
+
+    def predict(self):
+        self.client.connect()
+        # TODO: consider adding a "waiting" image while waiting for results.
+        # We should probably use a different thread/Future to wait for the prediction result, so the GUI won't be stuck
+        
+        prediction_raw = self.client.get_prediction_data()
+        print("Matalb result: {}".format(prediction_raw))
+        result_name = CLASS_TO_RESULT[prediction_raw]
+        print("Result name: {}".format(result_name))
+
+        prediction_img_path = self.images_paths[result_name]
+        self.ids["prediction_image"].source = prediction_img_path
+        time.sleep(1) # Sleep to avoid abrupt connection closing
         self.client.close_connection()
+
+    def reset_screen(self):
+        self.client.close_connection()
+        self.ids["prediction_image"].source = self.images_paths["start"]
+        self.ids["start_stop_button"].text = 'Start'
+        
 
 
 class CoAdaptiveScreen(Screen):
@@ -87,8 +104,10 @@ class OfflineTraining(MDScreen):
     pass
 
 class MIMainApp(MDApp):
-    def __init__(self, **kwargs):
+    def __init__(self, class_to_result, images_base_path, **kwargs):
         super().__init__(**kwargs)
+        self.class_to_result = class_to_result
+        self.images_base_path = images_base_path
 
     def build(self):
         # Create the screen manager
@@ -100,18 +119,23 @@ class MIMainApp(MDApp):
         sm.add_widget(OfflineTraining(name='offline'))
         sm.add_widget(ApplicationScreen(name='application'))
         # TODO: remove PredictionScreen later
-        sm.add_widget(PredictionScreen(name='prediction'))
+        sm.add_widget(PredictionScreen(class_to_results=self.class_to_result, images_base_path=self.images_base_path, name='prediction'))
 
         self.root_widget = sm
 
         return sm
 
 
- 
+#TODO: change according to used classes
+CLASS_TO_RESULT = {2: "yes", 3: "no"}
+import os.path
+current_file_path = os.path.abspath(os.path.dirname(__file__))
+IMAGES_BASE_PATH = os.path.join(current_file_path, "images")
+
 if __name__ == '__main__':     
     # Here the class MyApp is initialized
     # and its run() method called.
-    sample_app = MIMainApp()
-    sample_app.run()
+    app = MIMainApp(CLASS_TO_RESULT, IMAGES_BASE_PATH)
+    app.run()
 
 
